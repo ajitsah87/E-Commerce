ff47047a0ace980516b62fdcbfe0a7760e158f98
Ajit Sah <132166669+ajitsah87@users.noreply.github.com>
2024-10-26 14:50:30 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ app.use(cookieParser())
 
 app.use(cors(
  {
-  origin : "https://ajit-e-com-frontend.vercel.app",
+  origin : ["https://ajit-e-com-frontend.vercel.app"],
   credentials : true
  }
 ));
