50a76490291c1a9be28a7ca0378817ec08975904
ajisah87 <sahajit119@gmail.com>
2024-10-27 13:09:38 +0530

    updated sameSite value

--- Backend/controller/user/userSignIn.js
+++ Backend/controller/user/userSignIn.js
@@ -22,7 +22,7 @@ async function userSignInController(req, res) {
       const tokenOption = {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'None',
+        sameSite: process.env.NODE_ENV !== 'development' ? 'None' : 'Strict',
         maxAge: 30 * 24 * 60 * 60 * 1000  // 30 days in milliseconds
       };
 
