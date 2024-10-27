a5621abc385ec3331e9d1c3df23bec68ce93ea31
Ajit Sah <132166669+ajitsah87@users.noreply.github.com>
2024-10-27 12:52:15 +0530

    Update userSignIn.js

--- Backend/controller/user/userSignIn.js
+++ Backend/controller/user/userSignIn.js
@@ -22,7 +22,7 @@ async function userSignInController(req, res) {
       const tokenOption = {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'strict',
+        sameSite: 'None',
         maxAge: 30 * 24 * 60 * 60 * 1000  // 30 days in milliseconds
       };
 
